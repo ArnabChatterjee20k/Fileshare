0fd3a82687cba6a45a5fd8c3cf8a47e10e2cbb83
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2024-04-28 23:18:02 +0530

    setup -- clerk and shadcn

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://giving-anchovy-92.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ],
+  };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "./ConvexClientProvider";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,10 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
-      </body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </body>
+      </html>
   );
 }
